68e26d46087b1755a145feac530f9938096f8808
Jason Lee <94644030+JXL884@users.noreply.github.com>
2026-02-17 14:29:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,9 @@ export default defineConfig({
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/Personalwebsitestructure/",
+});
